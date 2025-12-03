3c387c53656be79ef70def939c940468b033a274
Dani <danibor2303@gmail.com>
2025-12-03 11:29:09 -0300

    Atualiza View da API equipes

--- api_equipes/views.py
+++ api_equipes/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Equipe
+from .serializers import EquipeSerializer
+from .permissions import IsProfessorOrCoordenador
+
+# POST /equipes/criar
+class CriarEquipeView(generics.CreateAPIView):
+    queryset = Equipe.objects.all()
+    serializer_class = EquipeSerializer
+    permission_classes = [IsAuthenticated, IsProfessorOrCoordenador]
+
+# DELETE /equipes/deletar/<id>
+class DeletarEquipeView(generics.DestroyAPIView):
+    queryset = Equipe.objects.all()
+    serializer_class = EquipeSerializer
+    permission_classes = [IsAuthenticated, IsProfessorOrCoordenador]
+    lookup_url_kwarg = "id_equipe"
+
+# GET /equipes/listar
+class ListarEquipesView(generics.ListAPIView):
+    queryset = Equipe.objects.all()
+    serializer_class = EquipeSerializer
+    permission_classes = [IsAuthenticated]
